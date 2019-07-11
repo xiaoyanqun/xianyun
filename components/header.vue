02029ecf7f9e34614eecfe2298078df0815ead84
肖燕群 <1129429967@qq.com>
2019-07-11 20:56:41 +0800

    实现了头部和底部布局

---
+++ components/header.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="container">
+    <el-row type="flex" justify="space-between" style="width:1000px;margin: 0 auto">
+      <div class="logo">
+        <nuxt-link to="/">
+          <img src="http://157.122.54.189:9093/images/logo.jpg" alt />
+        </nuxt-link>
+      </div>
+      <el-row type="flex" class="main">
+        <nuxt-link to="/">首页</nuxt-link>
+        <nuxt-link to="/post">旅游攻略</nuxt-link>
+        <nuxt-link to="/hotel">酒店</nuxt-link>
+        <nuxt-link to="/air">国内机票</nuxt-link>
+      </el-row>
+      <div class="login" v-if="false">
+        <nuxt-link to="/user/login">登录 / 注册</nuxt-link>
+      </div>
+      <div class="logout" v-if="true">
+        <el-dropdown>
+          <span class="el-dropdown-link">
+            <img src="https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1562849498&di=b2f08b6582e121a6014fb1cdc3200cac&src=http://b-ssl.duitang.com/uploads/item/201710/07/20171007171838_Fe5YN.thumb.700_0.jpeg" alt="">
+            赵丽颖
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>个人中心</el-dropdown-item>
+            <el-dropdown-item>退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang='less' scoped>
+.container {
+  height: 60px;
+  box-shadow: 0 3px 3px #ccc;
+  line-height: 60px;
+  .main {
+    flex: 1;
+    a {
+      display: block;
+      height: 60px;
+      box-sizing: border-box;
+      padding: 0 20px;
+      &:hover {
+        color: #409eff;
+        border-bottom: 5px #409eff solid;
+      }
+      &.nuxt-link-exact-active {
+        background-color: #409eff;
+        color: #fff;
+        &:hover {
+          color: #fff;
+        }
+      }
+    }
+  }
+  .logo {
+    margin-right: 20px;
+    img {
+      margin-top: 9px;
+      width: 156px;
+      height: 42px;
+    }
+  }
+  .logout{
+    img{
+      width: 36px;
+      height: 36px;
+      vertical-align:middle;
+    }
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,21 @@
 <template>
   <div>
+    <Header/>
     <nuxt />
+    <Footer/>
   </div>
 </template>
+<script>
+import Header from '@/components/header.vue'
+import Footer from '@/components/footer.vue'
+export default {
+  
+  components:{
+    Header,
+    Footer
+  }
+}
+</script>
 
 <style>
 html {
@@ -17,39 +30,19 @@ html {
   box-sizing: border-box;
 }
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
+*{
   margin: 0;
+  padding: 0;
+  
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
+ul,li,ol{
+  list-style: none;
+}
+a{
   text-decoration: none;
-  padding: 10px 30px;
+  color:inherit
 }
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+em,i{
+  font-style:normal;
 }
 </style>
