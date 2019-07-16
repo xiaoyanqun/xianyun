eba03697c42df45ef41d3567f324fa856b429fde
肖燕群 <1129429967@qq.com>
2019-07-16 21:09:57 +0800

    实现了机票列表和分页

---
+++ pages/air/flights.vue
@@ -0,0 +1,102 @@
+<template>
+  <section class="contianer">
+    <el-row type="flex" justify="space-between">
+      <!-- 顶部过滤列表 -->
+      <div class="flights-content">
+        <!-- 过滤条件 -->
+        <div></div>
+
+        <!-- 航班头部布局 -->
+        <div>
+          <FlightsListHead />
+        </div>
+
+        <!-- 航班信息 -->
+        <div>
+          <FlightsItem :data="item" v-for="(item,index) in tempList" :key="index" />
+        </div>
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="pageIndex"
+          :page-sizes="[5, 10, 15, 20]"
+          :page-size="pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+        ></el-pagination>
+      </div>
+
+      <!-- 侧边栏 -->
+      <div class="aside">
+        <!-- 侧边栏组件 -->
+      </div>
+    </el-row>
+  </section>
+</template>
+
+<script>
+import FlightsListHead from "@/components/air/flightsListHead.vue";
+import FlightsItem from "@/components/air/flightsItem.vue";
+export default {
+  data() {
+    return {
+      airList: {},
+      tempList:[],
+      pageIndex: 1,
+      pageSize: 5,
+      total: 5,
+    };
+  },
+  methods: {
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+      this.pageIndex = 1
+      this.pageSize = val
+      this.setDataList()
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+      this.pageIndex = val
+      this.setDataList()
+    },
+    // 显示页面数据
+    setDataList(){
+       this.tempList = this.airList.flights.slice(
+        (this.pageIndex - 1)*this.pageSize,
+        this.pageIndex *this.pageSize
+      )
+    }
+  },
+  components: {
+    FlightsListHead,
+    FlightsItem
+  },
+  mounted() {
+    this.$axios({
+      url: "/airs",
+      params: this.$route.query
+    }).then(res => {
+      console.log(res);
+      this.airList = res.data;
+      this.total = res.data.total
+      this.setDataList()
+    });
+  }
+};
+</script>
+
+<style scoped lang="less">
+.contianer {
+  width: 1000px;
+  margin: 20px auto;
+}
+
+.flights-content {
+  width: 745px;
+  font-size: 14px;
+}
+
+.aside {
+  width: 240px;
+}
+</style>
